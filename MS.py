2153ad3469b57f7e69e527d1efc7c8eb6f87cd24
MattTheArcherCat <45662250+MattTheArcherCat@users.noreply.github.com>
2018-12-06 10:32:24 -0500

    Add files via upload

---
+++ MS.py
@@ -0,0 +1,170 @@
+import webbrowser as wb
+import pyautogui as pg
+import time as t
+point = 0
+show = pg.prompt("What is your favorite show? ")
+if show == "THE SUPER MARIO BROTHER'S SUPER SHOW!!!":
+    wb.open("https://www.youtube.com/watch?v=vkQTgjGboWQ")
+    point += 5
+    t.sleep(3)
+    pg.alert("That's a SO NICE!")
+elif show == "Friends":
+    wb.open("https://www.youtube.com/watch?v=vQtveTUZrS8")
+    point -= 5
+    t.sleep(3)
+    pg.alert("You have none.")
+elif show == "yee":
+    pg.alert("yee")
+    point += 100
+    t.sleep(3)
+    wb.open == ("https://www.youtube.com/watch?v=q6EoRBvdVPQ")
+elif show == "i hate tv":
+    t.sleep(3)
+    pg.alert ("i hate u")
+    point -= 10
+elif show == "Wreck It Ralph 2":
+    t.sleep(3)
+    point += 20
+    wb.open("https://www.google.com/search?q=wreck+it+ralph+2&rlz=1C1GCEA_enUS752US752&source=lnms&tbm=isch&sa=X&ved=0ahUKEwif8eavo7PeAhUhnuAKHW69BzMQ_AUIECgD&biw=1366&bih=626#imgrc=jyDhSTWwa4QxXM:")
+elif show == "Ninja Fortnite Streams":
+    wb.open("https://www.twitch.tv/ninja")
+    t.sleep(5)
+    point += 40
+    pg.alert("Yeah Boi yeet wut omgosh noscope wombocombo doublepumped wrecked #SOOOOOO      COOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOL!!!!!!!!!!11!11!!!1!!")
+else: pg.alert("Your favorite show is " + show)
+thomasTheDankEngine = pg.prompt("What is the spiciest meme? ")
+if thomasTheDankEngine == "Uganda Knuckles":
+    wb.open("https://www.youtube.com/watch?v=X36wC-bDue4")
+    t.sleep(5)
+    point += 5
+    pg.alert("U Do Not No De Wey")
+elif thomasTheDankEngine == "Thomas The Train":
+    wb.open("https://www.youtube.com/watch?v=GnrwM7vFn_U")
+    t.sleep(5)
+    point += 10
+    pg.alert("Do-Do-Do-Do Do-Do DOO")
+elif thomasTheDankEngine == "We Are Number One":
+    wb.open("https://youtu.be/PfYnvDL0Qcw")
+    t.sleep(5)
+    point += 20
+    pg.alert("Yes you are.")
+elif thomasTheDankEngine == "No":
+    pg.alert ("No")
+    point += 1
+elif thomasTheDankEngine == "yee":
+    wb.open("https://www.youtube.com/watch?v=RRQwM4RYyyQ")
+    t.sleep(5)
+    point += 100
+    pg.alert("Yee.")
+else: pg.alert("Spicy!")
+alexander = pg.prompt("What is your name?")
+if alexander == "Alexander":
+    wb.open("http://www.videocally.com/Alex%20spizzirri/musically-account/24552622")
+    t.sleep(5)
+    point += 50
+    pg.alert("Hello.")
+elif alexander == "yee":
+    wb.open("https://www.youtube.com/watch?v=q6EoRBvdVPQ")
+    t.sleep(5)
+    point += 100
+    pg.alert("yee")
+elif alexander == "Berry B. Benson":
+    wb.open("https://www.youtube.com/watch?v=tlssD4pJpBk")
+    t.sleep(3)
+    wb.open("https://www.youtube.com/watch?v=RRQwM4RYyyQ")
+    t.sleep(3)
+    point += 100.00005
+    pg.alert("The bee movie but anytime someone flaps their wings play the entire bee movie video game walkthrough.")
+elif alexander == "Steve":
+    wb.open("https://www.youtube.com/channel/UCLmgD9bQ6q9okrWrfx9i56g")
+    t.sleep(5)
+    point += 35
+    pg.alert("Minecraft Steve?")
+elif alexander == "BeeJesus":
+    wb.open("https://www.youtube.com/watch?v=v_MZvX0wn5g")
+    t.sleep(1)
+    point = point * 2
+    pg.alert("BeeJesus.")
+else: pg.alert("Sup.")
+yaboibrandon = pg.prompt("Who is the coolest battle cats player you know?")
+if yaboibrandon == "Matthew":
+    point += 20
+    wb.open("https://www.youtube.com/watch?v=rog8ou-ZepE")
+    t.sleep(5)
+    pg.alert("Yessssssssss.")
+elif yaboibrandon == "That kid that once that I saw somewhere":
+    t.sleep(5)
+    point += 5
+    wb.open("https://www.google.com/search?q=that+kid&rlz=1C1GCEA_enUS752US752&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi465rMjcXeAhXhg-AKHer0DEIQ_AUIEygB&biw=681&bih=616#imgrc=YtI_BmlaclhH1M:")
+    pg.alert("Is this him.")
+elif yaboibrandon == "yee":
+    wb.open("https://www.youtube.com/watch?v=RknrCioITXg")
+    t.sleep(5)
+    point += 100
+    pg.alert("yee is eey spelled backwards.")
+elif yaboibrandon == "Me":
+    wb.open("https://www.youtube.com/watch?v=rog8ou-ZepE")
+    t.sleep(5)
+    point += 30
+    pg.alert("Cool.")
+elif yaboibrandon =="what":
+    point -= 60
+    pg.alert(".....................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................What?")
+else: pg.alert("Ok")
+e = pg.prompt("e")
+if e == "e":
+    pg.alert ("e")
+    point += 50
+elif e == "bee":
+    wb.open("https://www.google.com/search?q=bee+movie+2&rlz=1C1GCEA_enUS752US752&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjJwN-hjMXeAhVKMt8KHUoWBG8Q_AUIFCgC&biw=681&bih=616#imgrc=3Ck3BnW3zNpTQM:")
+    t.sleep(5)
+    point += 80
+    pg.alert("BEE")
+elif e == "illuminati":
+    wb.open("https://www.google.com/search?q=illuminati&rlz=1C1GCEA_enUS752US752&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj-o87ko7PeAhVnS98KHR-oB_UQ_AUIDigB&biw=1366&bih=626#imgrc=XbPHvzyC_ItbBM:")
+    wb.open("https://en.wikipedia.org/wiki/Illuminati")
+    t.sleep(1)
+    wb.open("https://www.theguardian.com/commentisfree/2017/nov/22/what-is-illuminati-google-autocomplete")
+    t.sleep(1)
+    wb.open("https://www.complex.com/pop-culture/2018/09/what-is-the-illuminati-conspiracy-and-who-are-its-members/")
+    wb.open("https://www.google.com/search?q=illuminati&rlz=1C1GCEA_enUS752US752&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj-o87ko7PeAhVnS98KHR-oB_UQ_AUIDigB&biw=1366&bih=626#imgrc=XbPHvzyC_ItbBM:")
+    t.sleep(1)
+    wb.open("https://en.wikipedia.org/wiki/Illuminati")
+    t.sleep(1)
+    wb.open("https://www.theguardian.com/commentisfree/2017/nov/22/what-is-illuminati-google-autocomplete")
+    wb.open("https://en.wikipedia.org/wiki/Illuminati")
+    t.sleep(1)
+    wb.open("https://www.theguardian.com/commentisfree/2017/nov/22/what-is-illuminati-google-autocomplete")
+    t.sleep(1)
+    wb.open("https://www.complex.com/pop-culture/2018/09/what-is-the-illuminati-conspiracy-and-who-are-its-members/")
+    t.sleep(1)
+    wb.open("https://www.google.com/search?q=illuminati&rlz=1C1GCEA_enUS752US752&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj-o87ko7PeAhVnS98KHR-oB_UQ_AUIDigB&biw=1366&bih=626#imgrc=XbPHvzyC_ItbBM:")
+    wb.open("https://en.wikipedia.org/wiki/Illuminati")
+    t.sleep(1)
+    wb.open("https://www.theguardian.com/commentisfree/2017/nov/22/what-is-illuminati-google-autocomplete")
+    wb.open("https://www.complex.com/pop-culture/2018/09/what-is-the-illuminati-conspiracy-and-who-are-its-members/")
+    pg.alert("gip si sdrowcab backwords is pig")
+    t.sleep(1)
+    pg.alert("Knowledge is power!")
+    point += 70
+elif e =="what":
+    wb.open("https://www.google.com/search?q=what&rlz=1C1GCEA_enUS752US752&oq=what&aqs=chrome..69i57j69i60l2j69i59j0l2.2748j0j7&sourceid=chrome&ie=UTF-8")
+    pg.alert ("what")
+    point += 0.1
+elif e =="k":
+    wb.open ("https://www.youtube.com/watch?v=OGVbUgqp7LQ")
+    t.sleep(1)
+    point = point*3
+    pg.alert ("k was the missing letter.")
+elif e =="minecraft":
+    wb.open("https://www.youtube.com/watch?v=dgha9S39Y6M")
+    t.sleep(1)
+    point += 14
+    pg.alert("MINING DIAMONDS MINE THEM EVERYDAY BECAUSE ITS FUN GOTTA GET THEM DIAMONDS FIGHTING CREEPY CREEPERS")
+elif e =="this isn't very creative.":
+    pg.alert("yes")
+else:
+    print ("oh okay")
+
+    
+pg.alert("You have a total of " + str(point) + " points.")
